39b4b5b95947ce74e6dab5c76207ef9be5372715
诺离 <lizhichao@n22.com.cn>
2020-05-07 18:53:40 +0800

    swift 项目初始化

---
+++ Swift-Learning/Swift-Learning/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by 诺离 on 2019/11/21.
+//  Copyright © 2019 HangZhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableview: UITableView!
+    var propertyList: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let property = getPropertyList() as? [String] else {
+            return
+        }
+        
+        propertyList = property
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.shadowImage = UIImage()
+    }
+
+    private func getPropertyList() -> Any? {
+        guard let ipaPlistBundlePath = Bundle.main.path(forResource: "FunctionList", ofType: "plist") else {
+            return nil
+        }
+        
+        let ipaPlistBundleUrl = URL(fileURLWithPath: ipaPlistBundlePath)
+        do {
+            let propertyData = try Data(contentsOf: ipaPlistBundleUrl)
+            let property = try PropertyListSerialization.propertyList(from: propertyData, options: [], format: nil)
+            return property
+        }catch{
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return propertyList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = propertyList[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    }
+}
+
